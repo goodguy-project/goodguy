1808de8b372998e4b7d713916099fb59facde006
ConanYu <c.zing@outlook.com>
2020-10-17 23:01:26 +0800

    feat: message.py负责消息处理

--- src/message.py
+++ src/message.py
@@ -1,4 +1,6 @@
-import json, requests
+import json, requests, config, common, time
+from codeforces import GetCodeforcesPromise, CodeforcesDataToString
+from promise import Promise
 
 def SendMessage(token, message_type, send_id, text=''):
   url = "https://open.feishu.cn/open-apis/message/v4/send/"
@@ -20,5 +22,54 @@ def SendMessage(token, message_type, send_id, text=''):
   except Exception as e:
     print(e)
 
+kMenu = '''1.查询用户Codeforces情况，样式：`cf 用户名`
+2.查询用户Atcoder情况，样式：`atc 用户名`
+'''
+
+def IsTimeOut(start_time):
+  now = common.GetTime()
+  return now - start_time > float(config.GetConfig('crawler', 'timeout', default=5)) * (10 ** 9)
+
+def HandleMessageThread(token, message_type, send_id, text=''):
+  global kMenu
+  try:
+    text_split = text.split()
+    f = '' if len(text_split) <= 0 else text_split[0]
+    handle = '' if len(text_split) <= 1 else text_split[1]
+    if f == '菜单':
+      SendMessage(token, message_type, send_id, text=kMenu)
+    elif f == 'reload_config':
+      config.ReloadConfig()
+      print('reload config successd.')
+    elif f in {'cf', 'CF', 'codeforces'}:
+      try:
+        start_time = common.GetTime()
+        promise = GetCodeforcesPromise(handle)
+        while not IsTimeOut(start_time) and not hasattr(promise, 'result'):
+          time.sleep(float(config.GetConfig('crawler', 'sleeptime', default=0.01)))
+        if hasattr(promise, 'result'):
+          SendMessage(token, message_type, send_id, text=CodeforcesDataToString(handle, promise.result))
+        else:
+          SendMessage(token, message_type, send_id, text=f'命令 {text} 超时')
+      except common.NoSuchUserException as e:
+        SendMessage(token, message_type, send_id, text=f'没有找到Codeforces用户 {handle}')
+    elif f in {'atc', 'ATC', 'atcoder'}:
+      pass
+    else:
+      SendMessage(token, message_type, send_id, text=f'未知命令 {text}')
+      print(f'unknown command {f}.')
+  except Exception as e:
+    print(e)
+
 def HandleMessage(token, message_type, send_id, text=''):
-  pass
+  Promise(HandleMessageThread, (token, message_type, send_id, text)).start()
+
+if __name__ == "__main__":
+  promise = GetCodeforcesPromise('ConanYu')
+  while not hasattr(promise, 'result'):
+    time.sleep(float(config.GetConfig('crawler', 'sleeptime', default=0.01)))
+  print(promise.result)
+  promise = GetCodeforcesPromise('ConanYu')
+  while not hasattr(promise, 'result'):
+    time.sleep(float(config.GetConfig('crawler', 'sleeptime', default=0.01)))
+  print(promise.result)
