50f598f684d847977805a8f615fe4e4ead5a5a0c
ConanYu <c.zing@outlook.com>
2020-10-21 14:37:06 +0800

    feat: 重构了cache的GetPromise逻辑并把代码转换成4空格缩进 todo: token和codeforces_contest的获取应该新弄一个缓存

--- src/listener.py
+++ src/listener.py
@@ -1,100 +1,105 @@
 import json, message, config, requests
-from os import path
-from urllib import request, parse
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from cache import Cache
 
+
 def GetTenantAccessToken():
-  url = "https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal/"
-  headers = {
-    "Content-Type" : "application/json"
-  }
-  req_body = {
-    "app_id": config.GetConfig("app", "id"),
-    "app_secret": config.GetConfig("app", "secret")
-  }
-  data = bytes(json.dumps(req_body), encoding='utf8')
-  print(data)
-  try:
-    req = requests.post(url=url, data=data, headers=headers)
-    req = json.loads(req.text)
-    return req.get('tenant_access_token', '')
-  except Exception as e:
-    print(e)
-    return ''
+    url = "https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal/"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    req_body = {
+        "app_id": config.GetConfig("app", "id"),
+        "app_secret": config.GetConfig("app", "secret")
+    }
+    data = bytes(json.dumps(req_body), encoding='utf8')
+    print(data)
+    try:
+        req = requests.post(url=url, data=data, headers=headers)
+        req = json.loads(req.text)
+        return req.get('tenant_access_token', '')
+    except Exception as e:
+        print(e)
+        return ''
+
 
 def GetTenantAccessTokenData(unused_var):
-  data = {
-    'access_token': GetTenantAccessToken()
-  }
-  return data
+    data = {
+        'access_token': GetTenantAccessToken()
+    }
+    return data
+
 
 # token过期时间设置为28分钟
+# TODO(ConanYu): 这里不应该使用Cache
 token_cache = Cache(GetTenantAccessTokenData, 1680)
 
+
 class RequestHandler(BaseHTTPRequestHandler):
-  def do_POST(self):
-    # 解析请求 body
-    req_body = self.rfile.read(int(self.headers['content-length']))
-    obj = json.loads(req_body.decode("utf-8"))
-    print(req_body)
-    # 校验 verification token 是否匹配，token 不匹配说明该回调并非来自开发平台
-    token = obj.get("token", "")
-    if token != config.GetConfig("app", "verification", "token"):
-      print("verification token not match, token =", token)
-      self.Response("")
-      return
-    # 根据 type 处理不同类型事件
-    type = obj.get("type", "")
-    if "url_verification" == type:  # 验证请求 URL 是否有效
-      self.HandleRequestUrlVerify(obj)
-    elif "event_callback" == type:  # 事件回调
-      # 获取事件内容和类型，并进行相应处理，此处只关注给机器人推送的消息事件
-      event = obj.get("event")
-      if event.get("type", "") == "message":
-        self.HandleMessage(event)
-        return
+    def do_POST(self):
+        # 解析请求 body
+        req_body = self.rfile.read(int(self.headers['content-length']))
+        obj = json.loads(req_body.decode("utf-8"))
+        print(req_body)
+        # 校验 verification token 是否匹配，token 不匹配说明该回调并非来自开发平台
+        token = obj.get("token", "")
+        if token != config.GetConfig("app", "verification", "token"):
+            print("verification token not match, token =", token)
+            self.Response("")
+            return
+        # 根据 type 处理不同类型事件
+        type = obj.get("type", "")
+        if "url_verification" == type:  # 验证请求 URL 是否有效
+            self.HandleRequestUrlVerify(obj)
+        elif "event_callback" == type:  # 事件回调
+            # 获取事件内容和类型，并进行相应处理，此处只关注给机器人推送的消息事件
+            event = obj.get("event")
+            if event.get("type", "") == "message":
+                self.HandleMessage(event)
+                return
 
-  def HandleRequestUrlVerify(self, post_obj):
-    # 原样返回 challenge 字段内容
-    challenge = post_obj.get("challenge", "")
-    rsp = {'challenge': challenge}
-    self.Response(json.dumps(rsp))
+    def HandleRequestUrlVerify(self, post_obj):
+        # 原样返回 challenge 字段内容
+        challenge = post_obj.get("challenge", "")
+        rsp = {'challenge': challenge}
+        self.Response(json.dumps(rsp))
 
-  def HandleMessage(self, event):
-    global token_cache
-    # 此处只处理 text 类型消息，其他类型消息忽略
-    msg_type = event.get("msg_type", "")
-    if msg_type != "text":
-      print("unknown msg_type =", msg_type)
-      return
-    # 调用发消息 API 之前，先要获取 API 调用凭证：tenant_access_token
-    token_promise = token_cache.GetPromise('')
-    token_promise.join()
-    access_token = ''
-    if hasattr(token_promise, 'result'):
-      access_token = token_promise.result.get('access_token', '')
-    if access_token == '':
-      return
-    if event.get('open_chat_id', None) is not None:
-      message.HandleMessage(access_token, 'chat_id', event.get('open_chat_id'), event.get("text_without_at_bot"))
-    elif event.get('open_id', None) is not None:
-      message.HandleMessage(access_token, 'open_id', event.get('open_id'), event.get('text_without_at_bot'))
-    else:
-      print('unknown message')
+    def HandleMessage(self, event):
+        global token_cache
+        # 此处只处理 text 类型消息，其他类型消息忽略
+        msg_type = event.get("msg_type", "")
+        if msg_type != "text":
+            print("unknown msg_type =", msg_type)
+            return
+        # 调用发消息 API 之前，先要获取 API 调用凭证：tenant_access_token
+        token_promise = token_cache.GetPromise('')
+        token_promise.join()
+        access_token = ''
+        if hasattr(token_promise, 'result'):
+            access_token = token_promise.result.get('access_token', '')
+        if access_token == '':
+            return
+        if event.get('open_chat_id', None) is not None:
+            message.HandleMessage(access_token, 'chat_id', event.get('open_chat_id'), event.get("text_without_at_bot"))
+        elif event.get('open_id', None) is not None:
+            message.HandleMessage(access_token, 'open_id', event.get('open_id'), event.get('text_without_at_bot'))
+        else:
+            print('unknown message')
 
-  def Response(self, body):
-    self.send_response(200)
-    self.send_header('Content-Type', 'application/json')
-    self.end_headers()
-    self.wfile.write(body.encode())
+    def Response(self, body):
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        self.wfile.write(body.encode())
+
 
 def run():
-  port = config.GetConfig("http", "port")
-  server_address = ('', port)
-  httpd = HTTPServer(server_address, RequestHandler)
-  print("EigawaNoa is running...")
-  httpd.serve_forever()
+    port = config.GetConfig("http", "port")
+    server_address = ('', port)
+    httpd = HTTPServer(server_address, RequestHandler)
+    print("EigawaNoa is running...")
+    httpd.serve_forever()
+
 
 if __name__ == '__main__':
-  run()
+    run()
--- src/message.py
+++ src/message.py
@@ -4,90 +4,97 @@ from atcoder import GetAtcoderPromise, AtcoderDataToString
 from codeforces_contest import GetCodeforcesUpcomingContestPromise, CodeforcesUpcomingContestDataToString
 from promise import Promise
 
+
 def SendMessage(token, message_type, send_id, text=''):
-  url = "https://open.feishu.cn/open-apis/message/v4/send/"
-  headers = {
-    "Content-Type": "application/json",
-    "Authorization": "Bearer " + token
-  }
-  req_body = {
-    message_type: send_id,
-    "msg_type": "text",
-    "content": {
-      "text": text
+    url = "https://open.feishu.cn/open-apis/message/v4/send/"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + token
     }
-  }
-  try:
-    data = bytes(json.dumps(req_body), encoding='utf-8')
-    req = requests.post(url=url, data=data, headers=headers)
-    print(req.text)
-  except Exception as e:
-    print(e)
+    req_body = {
+        message_type: send_id,
+        "msg_type": "text",
+        "content": {
+            "text": text
+        }
+    }
+    try:
+        data = bytes(json.dumps(req_body), encoding='utf-8')
+        req = requests.post(url=url, data=data, headers=headers)
+        print(req.text)
+    except Exception as e:
+        print(e)
+
 
 kMenu = '''1.查询用户Codeforces情况，样式：`cf 用户名`
 2.查询用户Atcoder情况，样式：`atc 用户名`
 3.查询Codeforces最近比赛，样式：`cf`'''
 
+
 def IsTimeOut(start_time):
-  now = common.GetTime()
-  return now - start_time > float(config.GetConfig('crawler', 'timeout', default=15)) * (10 ** 9)
+    now = common.GetTime()
+    return now - start_time > float(config.GetConfig('crawler', 'timeout', default=15)) * (10 ** 9)
+
 
 def HandleMessageThread(token, message_type, send_id, text=''):
-  global kMenu
-  try:
-    if text is None:
-      text = ''
-    text_split = text.split()
-    f = '' if len(text_split) <= 0 else text_split[0]
-    handle = '' if len(text_split) <= 1 else text_split[1]
-    if f in {'菜单', 'menu', ''}:
-      SendMessage(token, message_type, send_id, text=kMenu)
-    elif f == 'reload_config':
-      config.ReloadConfig()
-      print('reload config successd.')
-    elif f in {'cf', 'CF', 'codeforces'}:
-      if handle == '':
-        start_time = common.GetTime()
-        promise = GetCodeforcesUpcomingContestPromise()
-        while not IsTimeOut(start_time) and not hasattr(promise, 'result'):
-          time.sleep(float(config.GetConfig('crawler', 'sleeptime', default=0.01)))
-        if hasattr(promise, 'result'):
-          SendMessage(token, message_type, send_id, text=CodeforcesUpcomingContestDataToString(promise.result))
+    global kMenu
+    try:
+        if text is None:
+            text = ''
+        text_split = text.split()
+        f = '' if len(text_split) <= 0 else text_split[0]
+        handle = '' if len(text_split) <= 1 else text_split[1]
+        if f in {'菜单', 'menu', ''}:
+            SendMessage(token, message_type, send_id, text=kMenu)
+        elif f == 'reload_config':
+            config.ReloadConfig()
+            print('reload config successd.')
+        elif f in {'cf', 'CF', 'codeforces'}:
+            if handle == '':
+                start_time = common.GetTime()
+                promise = GetCodeforcesUpcomingContestPromise()
+                while not IsTimeOut(start_time) and not hasattr(promise, 'result'):
+                    time.sleep(float(config.GetConfig('crawler', 'sleeptime', default=0.01)))
+                if hasattr(promise, 'result'):
+                    SendMessage(token, message_type, send_id,
+                                text=CodeforcesUpcomingContestDataToString(promise.result))
+                else:
+                    SendMessage(token, message_type, send_id, text=f'命令 {text} 超时')
+            else:
+                start_time = common.GetTime()
+                promise = GetCodeforcesPromise(handle.lower())
+                while not IsTimeOut(start_time) and not hasattr(promise, 'result'):
+                    time.sleep(float(config.GetConfig('crawler', 'sleeptime', default=0.01)))
+                if hasattr(promise, 'result'):
+                    SendMessage(token, message_type, send_id, text=CodeforcesDataToString(handle, promise.result))
+                else:
+                    SendMessage(token, message_type, send_id, text=f'命令 {text} 超时或无法找到该用户')
+        elif f in {'atc', 'ATC', 'atcoder'}:
+            start_time = common.GetTime()
+            promise = GetAtcoderPromise(handle.lower())
+            while not IsTimeOut(start_time) and not hasattr(promise, 'result'):
+                time.sleep(float(config.GetConfig('crawler', 'sleeptime', default=0.01)))
+            if hasattr(promise, 'result'):
+                SendMessage(token, message_type, send_id, text=AtcoderDataToString(handle, promise.result))
+            else:
+                SendMessage(token, message_type, send_id, text=f'命令 {text} 超时或无法找到该用户')
         else:
-          SendMessage(token, message_type, send_id, text=f'命令 {text} 超时')
-      else:
-        start_time = common.GetTime()
-        promise = GetCodeforcesPromise(handle.lower())
-        while not IsTimeOut(start_time) and not hasattr(promise, 'result'):
-          time.sleep(float(config.GetConfig('crawler', 'sleeptime', default=0.01)))
-        if hasattr(promise, 'result'):
-          SendMessage(token, message_type, send_id, text=CodeforcesDataToString(handle, promise.result))
-        else:
-          SendMessage(token, message_type, send_id, text=f'命令 {text} 超时或无法找到该用户')
-    elif f in {'atc', 'ATC', 'atcoder'}:
-      start_time = common.GetTime()
-      promise = GetAtcoderPromise(handle.lower())
-      while not IsTimeOut(start_time) and not hasattr(promise, 'result'):
-        time.sleep(float(config.GetConfig('crawler', 'sleeptime', default=0.01)))
-      if hasattr(promise, 'result'):
-        SendMessage(token, message_type, send_id, text=AtcoderDataToString(handle, promise.result))
-      else:
-        SendMessage(token, message_type, send_id, text=f'命令 {text} 超时或无法找到该用户')
-    else:
-      SendMessage(token, message_type, send_id, text=f'未知命令 {text}，用法：\n{kMenu}')
-      print(f'unknown command {f}.')
-  except Exception as e:
-    print(e)
+            SendMessage(token, message_type, send_id, text=f'未知命令 {text}，用法：\n{kMenu}')
+            print(f'unknown command {f}.')
+    except Exception as e:
+        print(e)
+
 
 def HandleMessage(token, message_type, send_id, text=''):
-  Promise(HandleMessageThread, (token, message_type, send_id, text)).start()
+    Promise(HandleMessageThread, (token, message_type, send_id, text)).start()
+
 
 if __name__ == "__main__":
-  promise = GetCodeforcesPromise('ConanYu')
-  while not hasattr(promise, 'result'):
-    time.sleep(float(config.GetConfig('crawler', 'sleeptime', default=0.01)))
-  print(promise.result)
-  promise = GetCodeforcesPromise('ConanYu')
-  while not hasattr(promise, 'result'):
-    time.sleep(float(config.GetConfig('crawler', 'sleeptime', default=0.01)))
-  print(promise.result)
+    promise = GetCodeforcesPromise('ConanYu')
+    while not hasattr(promise, 'result'):
+        time.sleep(float(config.GetConfig('crawler', 'sleeptime', default=0.01)))
+    print(promise.result)
+    promise = GetCodeforcesPromise('ConanYu')
+    while not hasattr(promise, 'result'):
+        time.sleep(float(config.GetConfig('crawler', 'sleeptime', default=0.01)))
+    print(promise.result)
