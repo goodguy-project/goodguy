dae47ad3701fed3b50b586f5a02cb1d94abeb302
ConanYu <c.zing@outlook.com>
2020-10-30 12:23:30 +0800

    feat: 更新readme

--- README.md
+++ README.md
@@ -6,8 +6,8 @@
 
 > 1.查询用户Codeforces情况，样式：`cf 用户名`  
 > 2.查询用户Atcoder情况，样式：`atc 用户名`  
-> 3.查询Codeforces最近比赛，样式：`cf`
-> 4.给该群添加提醒，样式：`notice`
-> 5.取消提醒，样式：`unnotice`
+> 3.查询Codeforces最近比赛，样式：`cf`  
+> 4.给该群添加提醒，样式：`notice`  
+> 5.取消提醒，样式：`unnotice`  
 
 ## 使用方法
--- src/converse.py
+++ src/converse.py
@@ -5,7 +5,9 @@ from atcoder import GetAtcoderPromise, AtcoderDataToString
 
 kMenu = '''1.查询用户Codeforces情况，样式：`cf 用户名`
 2.查询用户Atcoder情况，样式：`atc 用户名`
-3.查询Codeforces最近比赛，样式：`cf`'''
+3.查询Codeforces最近比赛，样式：`cf`
+4.给该群添加提醒，样式：`notice`
+5.取消提醒，样式：`unnotice`'''
 
 
 def GetFromPromise(future, expire, data_to_string_func, argv=()):
