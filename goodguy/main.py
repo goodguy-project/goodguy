c6c9bda96353d1a574fb2ff6b1350bbfc31212d4
ConanYu <c.zing@outlook.com>
2021-08-13 00:13:52 +0800

    feat: add loguru support

--- goodguy/main.py
+++ goodguy/main.py
@@ -1,12 +1,18 @@
 import asyncio
 import logging
+import os
+
+from loguru import logger
 
 from goodguy.feishu.serve import serve as feishu_serve
 from goodguy.timer.timer import async_timer
 from goodguy.util.config import GLOBAL_CONFIG
+from goodguy.util.const import ROOT
 
 
 async def main():
+    logger.add(os.path.join(ROOT, 'goodguy.log'),
+               retention='14 days', rotation='12:00')
     logging.getLogger().setLevel(GLOBAL_CONFIG.get("loglevel", logging.ERROR))
     async_timer()
     await feishu_serve()
---
+++ goodguy/util/const.py
@@ -0,0 +1,6 @@
+import os
+
+ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+if __name__ == '__main__':
+    print(locals())
