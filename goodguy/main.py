375a05792a6d56996a4baa6d7642e687b5993a44
ConanYu <c.zing@outlook.com>
2021-08-13 00:20:31 +0800

    feat: add loguru support

--- goodguy/main.py
+++ goodguy/main.py
@@ -10,10 +10,20 @@ from goodguy.util.config import GLOBAL_CONFIG
 from goodguy.util.const import ROOT
 
 
-async def main():
-    logger.add(os.path.join(ROOT, 'goodguy.log'),
-               retention='14 days', rotation='12:00')
+def log_init():
+    level = {
+        logging.CRITICAL: 'CRITICAL',
+        logging.ERROR: 'ERROR',
+        logging.WARNING: 'WARNING',
+        logging.INFO: 'INFO',
+        logging.DEBUG: 'DEBUG',
+    }.get(GLOBAL_CONFIG.get("loglevel", logging.ERROR), 'ERROR')
+    logger.add(os.path.join(ROOT, 'goodguy.log'), retention='14 days', rotation='12:00', level=level)
     logging.getLogger().setLevel(GLOBAL_CONFIG.get("loglevel", logging.ERROR))
+
+
+async def main():
+    log_init()
     async_timer()
     await feishu_serve()
 
