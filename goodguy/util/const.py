4c7d109fb7cf182e4a593ff12bce3bfbe48dbd21
ConanYu <33613505+ConanYu@users.noreply.github.com>
2021-08-26 20:39:03 +0800

    feat: const.py add PLATFORM_ALL
    platform_all.py will be deleted

--- goodguy/util/const.py
+++ goodguy/util/const.py
@@ -2,5 +2,7 @@ import os
 
 ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+PLATFORM_ALL = ['codeforces', 'nowcoder', 'atcoder', 'leetcode']
+
 if __name__ == '__main__':
     print(locals())
