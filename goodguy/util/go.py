b0b47de98b528c4706a09100bcce3ad952a4084a
ConanYu <33613505+ConanYu@users.noreply.github.com>
2021-09-03 15:23:13 +0800

    feat: go decorator can use for class

--- goodguy/util/go.py
+++ goodguy/util/go.py
@@ -1,4 +1,10 @@
+# 异步函数装饰器
+# 加上了该装饰器后函数从同步变成异步
+# 如果是类加上了该装饰器后这个类的使用__getattribute__调用的函数都会变成异步
+# 异步的函数返回_Promise类对象 使用get函数可以获取结果
+import functools
 import threading
+from inspect import isroutine
 from typing import Callable, Union
 
 
@@ -25,15 +31,31 @@ class _Promise(object):
         return self.__thread.res
 
 
+def _decorator_function(func: Callable, daemon: bool) -> Callable:
+    def wrapper(*args, **kwargs) -> _Promise:
+        return _Promise(_Thread(func, daemon, args, kwargs))
+
+    return wrapper
+
+
+def _decorator_class(cls: type, daemon: bool) -> Callable:
+    class _GoDecorator(cls):
+        def __getattribute__(self, item):
+            what = super().__getattribute__(item)
+            if isroutine(what):
+                return _decorator_function(what, daemon)
+            return what
+
+    return _GoDecorator
+
+
+def _decorator(obj: Callable, daemon: bool) -> Callable:
+    if isinstance(obj, type):
+        return _decorator_class(obj, daemon)
+    return _decorator_function(obj, daemon)
+
+
 def go(daemon: Union[bool, Callable] = True):  # pylint: disable=invalid-name
-    def decorator(func: Callable):
-        def wrapper(*args, **kwargs) -> _Promise:
-            return _Promise(_Thread(func, daemon, args, kwargs))
-
-        return wrapper
-
     if callable(daemon):
-        f = daemon
-        daemon = True
-        return decorator(f)
-    return decorator
+        return _decorator(daemon, True)
+    return functools.partial(_decorator, daemon=daemon)
