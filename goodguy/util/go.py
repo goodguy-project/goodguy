c2f0d05c9018d93117337f86e9d8dfebdc05bb97
zmm <2191482447@qq.com>
2021-09-15 23:20:39 +0800

    feat: process suppoort for go decorator

--- goodguy/util/go.py
+++ goodguy/util/go.py
@@ -6,6 +6,7 @@ import functools
 import threading
 from inspect import isroutine
 from typing import Callable, Union
+import multiprocessing as mp
 
 
 class _Thread(threading.Thread):
@@ -21,6 +22,23 @@ class _Thread(threading.Thread):
         self.res = self.__func(*self.__args, **self.__kwargs)
 
 
+class _Process(mp.Process):
+    def __init__(self, func: Callable, daemon: bool, args, kwargs):
+        super().__init__()
+        self.__func = func
+        self.__args = args
+        self.__kwargs = kwargs
+        self.res = None
+        self.que = mp.Queue()
+
+    def run(self) -> None:
+        self.que.put(self.__func(*self.__args, **self.__kwargs))
+
+    def join(self):
+        self.res = self.que.get()
+        super().join()
+
+
 class _Promise(object):
     def __init__(self, thread: _Thread):
         self.__thread = thread
@@ -31,31 +49,34 @@ class _Promise(object):
         return self.__thread.res
 
 
-def _decorator_function(func: Callable, daemon: bool) -> Callable:
+def _decorator_function(func: Callable, daemon: bool, use_process: bool) -> Callable:
     def wrapper(*args, **kwargs) -> _Promise:
-        return _Promise(_Thread(func, daemon, args, kwargs))
+        if use_process:
+            return _Promise(_Process(func, daemon, args, kwargs))
+        else:
+            return _Promise(_Thread(func, daemon, args, kwargs))
 
     return wrapper
 
 
-def _decorator_class(cls: type, daemon: bool) -> Callable:
+def _decorator_class(cls: type, daemon: bool, use_process: bool) -> Callable:
     class _GoDecorator(cls):
         def __getattribute__(self, item):
             what = super().__getattribute__(item)
             if isroutine(what):
-                return _decorator_function(what, daemon)
+                return _decorator_function(what, daemon, use_process)
             return what
 
     return _GoDecorator
 
 
-def _decorator(obj: Callable, daemon: bool) -> Callable:
+def _decorator(obj: Callable, daemon: bool, use_process: bool) -> Callable:
     if isinstance(obj, type):
-        return _decorator_class(obj, daemon)
-    return _decorator_function(obj, daemon)
+        return _decorator_class(obj, daemon, use_process)
+    return _decorator_function(obj, daemon, use_process)
 
 
-def go(daemon: Union[bool, Callable] = True):  # pylint: disable=invalid-name
+def go(daemon: Union[bool, Callable] = True, use_process: bool = False):  # pylint: disable=invalid-name
     if callable(daemon):
-        return _decorator(daemon, True)
-    return functools.partial(_decorator, daemon=daemon)
+        return _decorator(daemon, True, use_process)
+    return functools.partial(_decorator, daemon=daemon, use_process=use_process)
